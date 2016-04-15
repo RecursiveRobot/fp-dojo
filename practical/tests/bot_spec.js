3942f90de95188864df4df3d789c19e39b20eb3a
Edward Steere <edward.steere@gmail.com>
2016-04-15 09:23:00 +0200

    Added test for one user.

--- practical/tests/bot_spec.js
+++ practical/tests/bot_spec.js
@@ -6,7 +6,8 @@ describe ("Bot test Suite", function (){
         assert.equal (ChatBot.initialGreeting ([]), "Hello, anyone there?");
         assert.equal (ChatBot.initialGreeting (['Bob', 'Fred']), "Hello Bob and Fred!");
         assert.equal (ChatBot.initialGreeting (['Bob', 'Fred', 'Alice']), "Hello Bob, Fred and Alice!");
-        assert.equal (ChatBot.initialGreeting (['Bob', 'Fred', 'Alice', 'Frank', 'Mary']), "Hello everyone!");
+        assert.equal (ChatBot.initialGreeting (['Bob', 'Fred', 'Alice', 'Frank', 'Mary']), "Hello everyone!");;
+        assert.equal (ChatBot.initiagGreeting (['Bob']), "Hello Bob!");
     });
     it ("Tokenise the message text.", function () {
         assert.deepEqual (ChatBot.tokenise ("This is a test.", ['This', "is", "a", "test", "."]));
@@ -15,46 +16,46 @@ describe ("Bot test Suite", function (){
                                             ["This", "message", "contains", "two", "sentances.", "Is", "one", "of", "them", "a", "question", "?"]));
     });
     it ('Parse Greeting', function () {
-    assert.deepEqual (ChatBot.parseGreeting (['This', "is", "a", "test", "."],
-                                             {'Remaining': ['This', "is", "a", "test", "."],
-                                              'Parsed': undefined}));
-    assert.deepEqual (ChatBot.parseGreeting ([],
-                                             {'Remaining': [],
-                                              'Parsed': undefined}));
-    assert.deepEqual (ChatBot.parseGreeting (["Is", "one", "of", "them", "a", "question", "?"],
-                                             {'Remaining': [],
-                                              'Parsed': {'Type': ChatBot.QUESTION,
-                                                         'Values': [{'Type': ChatBot.KEYWORD,
-                                                                     'Value': 'Is'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'one'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'of'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'them'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'a'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'question'},
-                                                                    {'Type': ChatBot.TERMINATOR,
-                                                                     'Value': '?'}]}}));
-    assert.deepEqual (ChatBot.parseGreeting (["Is", "one", "of", "them", "a", "question", "?",
-                                              "Here", "is", "a", "statement", "."],
-                                             {'Remaining': ["Here", "is", "a", "statement", "."],
-                                              'Parsed': {'Type': ChatBot.QUESTION,
-                                                         'Values': [{'Type': ChatBot.KEYWORD,
-                                                                     'Value': 'Is'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'one'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'of'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'them'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'a'},
-                                                                    {'Type': ChatBot.OBJECT,
-                                                                     'Value': 'question'},
-                                                                    {'Type': ChatBot.TERMINATOR,
-                                                                     'Value': '?'}]}}));
-});
+        assert.deepEqual (ChatBot.parseGreeting (['This', "is", "a", "test", "."],
+                                                 {'Remaining': ['This', "is", "a", "test", "."],
+                                                  'Parsed': undefined}));
+        assert.deepEqual (ChatBot.parseGreeting ([],
+                                                 {'Remaining': [],
+                                                  'Parsed': undefined}));
+        assert.deepEqual (ChatBot.parseGreeting (["Is", "one", "of", "them", "a", "question", "?"],
+                                                 {'Remaining': [],
+                                                  'Parsed': {'Type': ChatBot.QUESTION,
+                                                             'Values': [{'Type': ChatBot.KEYWORD,
+                                                                         'Value': 'Is'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'one'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'of'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'them'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'a'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'question'},
+                                                                        {'Type': ChatBot.TERMINATOR,
+                                                                         'Value': '?'}]}}));
+        assert.deepEqual (ChatBot.parseGreeting (["Is", "one", "of", "them", "a", "question", "?",
+                                                  "Here", "is", "a", "statement", "."],
+                                                 {'Remaining': ["Here", "is", "a", "statement", "."],
+                                                  'Parsed': {'Type': ChatBot.QUESTION,
+                                                             'Values': [{'Type': ChatBot.KEYWORD,
+                                                                         'Value': 'Is'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'one'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'of'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'them'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'a'},
+                                                                        {'Type': ChatBot.OBJECT,
+                                                                         'Value': 'question'},
+                                                                        {'Type': ChatBot.TERMINATOR,
+                                                                         'Value': '?'}]}}));
+    });
 });
