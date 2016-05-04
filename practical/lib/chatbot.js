27cf509fdb0189557e36d714adfaf97581fa55ff
Edward Steere <edward.steere@gmail.com>
2016-05-04 17:48:38 +0200

    Simplified practical.

--- practical/lib/chatbot.js
+++ practical/lib/chatbot.js
@@ -1,7 +1,18 @@
-var Util = require ('./util');
+var Util = require ('../lib/util');
 var _ = require ('ramda');
 
-/* The slack bot we'll write will only understand bot-lang: a very
+/* NOTE:
+ * The exercises appear out of order in this file.
+ * To begin, search for the comment 'Part one'
+ * To continue with the practical simply search for 'Part two' etc.
+ */
+
+/* NOTE: (as with the exercises)
+ * We've provided some starting points to make things easier.
+ * Replace the question marks with code to implement the function.
+ */
+
+/* The chat bot we'll write will only understand bot-lang: a very
  * strict and highly simplified version of English. The core of
  * bot-lang is UnderstoodMessage.
  */
@@ -18,7 +29,7 @@ var _ = require ('ramda');
  * [{'Type': 'Greeting', 'Values':
  *      [{'Type': 'Keyword', 'Value': 'Hi'},
  *       {'Type': 'Object', 'Value': 'Edward'},
- *       {'Type': 'Terminal', 'Value': '.'}]}]
+ *       {'Type': 'Terminal', 'Value': '.'}]}..]
  *
  * The UnderstoodMessage above consists of a single ComplexObject
  * which as it's values has three SimpleObjects.
@@ -26,23 +37,113 @@ var _ = require ('ramda');
  * The types which our tree can hold are listed as follows:
  */
 // These objects are complex and therefore have 'Values';
-var GREETING = 'Greeting';
+var GREETING  = 'Greeting';
 var STATEMENT = 'Statement';
-var QUESTION = 'Question';
+var QUESTION  = 'Question';
 
 // These objects are simple and therefore have a 'Value';
-var KEYWORD = 'Keyword';
-var OBJECT = 'Object';
+var KEYWORD  = 'Keyword';
+var OBJECT   = 'Object';
 var TERMINAL = 'Terminal';
+
+/* (String, ComplexObject) -> String
+ *
+ * Part Eight: Implement respond using the appropriate evaluation
+ * functions.
+ */
+var respond = _.curry (function (reply, value) {
+    if (value.Type == GREETING){
+        return reply + evaluateGreeting (value);
+    } else if (value.Type == STATEMENT) {
+        return reply + evaluateStatement (value);
+    } else if (value.Type == QUESTION) {
+        return reply + evaluateQuestion (value);
+    } else {
+        return reply;
+    }
+});
+
+/* [ComplexObject] -> String
+ *
+ * Part Six: Implement evaluate by collecting the results of evaluating each
+ * element of the [ComplexObject] in a single string message to be
+ * sent back to the user.
+ *
+ * Hint: Use reduce and the functions which follow.
+ */
+var evaluate = _.reduce (respond, "");
+
+/* String -> [String]
+ *
+ * Rules:
+ *  - Tokens are separated by spaces except in the case of punctuation.
+ *  - Only alpha characters and punctuation is allowed in the token
+ *    list.
+ *  - Repeated spaces should be collapsed so that tokens separated by
+ *    repeated spaces only create a single token.
+ *
+ * Part Three: implement tokenize
+ */
+// Some useful regular expressions:
+var MATCH_WHITESPACE             = / +/g;
+var MATCH_PUNCTUATION            = /([.!?])/g;
+var MATCH_NON_ACCEPTED_CHARACTER = /[^a-zA-Z.!? ]/g;
+
+var splitOutPunctuation = function (toSplit) {
+    return ???;
+};
+
+var filterNonAcceptedCharacters = function (toFilter) {
+    return regexReplace (MATCH_NON_ACCEPTED_CHARACTERS, '');
+};
+
+var collapseSuccessiveWhitespaces = function (toFilter) {
+    return ???;
+};
+
+var regexReplace = _.curry (function (pattern, replaceWith, x) {
+    return x.replace (pattern, replaceWith);
+});
+
+var tokenise = _.compose (_.filter (_.compose (_.not, _.equals (""))),
+                          _.split (' '),
+                          collapseSuccessiveWhitespaces,
+                          ???,
+                          ???);
+
+/* [String] -> [ComplexObject]
+ *
+ * Part four:
+ * Hint: try each complex parser (parsers which read complex types)
+ * and use the first one which has any result, if none do then we can
+ * return an empty list, because we couldn't parse anything.
+ *
+ * Further Hint: Use recursion.
+ */
+function parseTokens (tokens) {
+    var firstDefined = _.compose (_.head, _.dropWhile (_.compose (_.equals (undefined), _.prop ('Parsed'))));
+    var parsed = firstDefined ([parseQuestion (tokens),
+                                parseGreeting (tokens),
+                                parseStatement (tokens)]);
+    if (parsed == undefined) {
+        return [];
+    }
+    return _.concat ([parsed.Parsed], parseTokens (parsed.Remaining));
+}
+
+/* String -> [ComplexObject]
+ *
+ * Part Two: implement parse.
+ * Hint: use composition, and look at type signatures.
+ */
+var parse = _.compose (???, ???);
 
 /* String -> String
  *
  * During the course of this exercise we will implement the pieces we
  * need to make our bot semi-literate.
  */
-function reply (message) {
-    return "";
-};
+var reply = _.compose (evaluate, parse);
 
 /* [String] -> String
  *
@@ -60,36 +161,19 @@ function reply (message) {
  *  - userNames = ['Bob', 'Fred', 'Alice', 'Frank', 'Mary']
  *  - returns "Hello everyone!"
  *  - Note: Any number of users >= 5 produces the same result
- * e.g. 5
- *  - userNames = ['Bob']
- *  - rettruns "Hello Bob!"
  */
 function initialGreeting (userNames) {
-    return "";
-};
-
-/* String -> [ComplexObject]
- *
- * Part Two: implement parse.
- * Hint: use composition, and look at type signatures.
- */
-function parse (messageText) {
-    return [];
-}
-
-/* String -> [String]
- *
- * Rules:
- *  - Tokens are separated by spaces except in the case of punctuation.
- *  - Only alpha characters and punctuation is allowed in the token
- *    list.
- *  - Repeated spaces should be collapsed so that tokens separated by
- *    repeated spaces only create a single token.
- *
- * Part Three: implement tokenize
- */
-function tokenise (messageText) {
-    return [];
+    if (userNames.length == 0) {
+        return ???;
+    } else if (userNames.length >= 5) {
+        return ???;
+    } else if (userNames.length == 1) {
+        return ???;
+    } else {
+        return "Hello "
+            + _.join (", ") (???)
+            + " and " + ??? + "!";
+    }
 }
 
 /* A parser returns a parsed object of the following schema:
@@ -105,20 +189,6 @@ function tokenise (messageText) {
  * [ComplexObject] and 'Remaining' should have tokens removed
  * corresponding to what was parsed.
  */
-
-/* [String] -> [ComplexObject]
- *
- * Part four:
- * Hint: try each complex parser (parsers which read complex types)
- * and use the first one which has any result, if none do then we can
- * assume that we are at a Terminal.
- *
- * Further Hint: Use recursion.
- */
-function parseTokens (tokens) {
-    return [{'Type': GREETING, 'Values':
-             [{'Type': TERMINAL, 'Value': '.'}]}];
-}
 
 var TERMINAL_KEYWORDS = ['.', '!', '?'];
 var QUESTION_START_KEYWORDS = ['How', 'Where', 'What', 'Why', 'Is'];
@@ -170,7 +240,7 @@ function parseTerminated (type, keywords) {
  */
 var parseQuestion = parseTerminated (QUESTION, QUESTION_START_KEYWORDS);
 
-var GREETING_KEYWORDS = ['Hi', 'Hello', 'Howzit', 'Hey'];
+var GREETING_START_KEYWORDS = ['Hi', 'Hello', 'Howzit', 'Hey'];
 
 /* [String] -> {'Remaining': [String], 'Parsed': ComplexObject}
  *
@@ -183,17 +253,13 @@ var GREETING_KEYWORDS = ['Hi', 'Hello', 'Howzit', 'Hey'];
  *
  * Part Four
  */
-function parseGreeting (tokens) {
-    return {'Type': GREETING, 'Values':
-            [{'Type': KEYWORD, 'Value': 'Hi'},
-             {'Type': OBJECT, 'Value': 'Edward'},
-             {'Type': TERMINAL, 'Value': '.'}]};
-}
+var parseGreeting  = parseTerminated (GREETING, GREETING_START_KEYWORDS);
 
 /* [String] -> {'Remaining': [String], 'Parsed': [ComplexObject]}
  *
  * Rule: A statement is a series of non-keyword tokens which are
- * terminated but don't start with a keyword.
+ * terminated but don't start with a keyword in
+ * particular. i.e. questions and greetings can be statements.
  *
  * e.g. ['This', 'is', 'a', 'statement', '.']
  *       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -202,38 +268,20 @@ function parseGreeting (tokens) {
  * Part Five
  */
 function parseStatement (tokens) {
-    return {'Type': STATEMENT,
-            'Values': [{'Type': OBJECT,
-                        'Value': 'This'}]};
-}
-
-/* [ComplexObject] -> String
- *
- * Part Six: Implement evaluate by collecting the results of evaluating each
- * element of the [ComplexObject] in a single string message to be
- * sent back to the user.
- *
- * Hint: Use reduce and the functions which follow.
- */
-function evaluate (complexObjects) {
-    return "";
-}
-
-/* (String, ComplexObject) -> String
- *
- * Part Eight: Implement respond using the appropriate evaluation
- * functions.
- */
-function respond (reply, value) {
-    if (value.Type == GREETING){
-        return reply + "";
-    } else if (value.Type == STATEMENT) {
-        return reply + "";
-    } else if (value.Type == QUESTION) {
-        return reply + "";
-    } else {
-        return reply;
+    var split = splitAtOneOfIgnoringCase (TERMINAL_KEYWORDS) (tokens);
+    var splitWithTerminal = [_.append (split[1][0], split[0]), _.slice (1, split[1].length, split[1])];
+    if (Util.isOneOfIgnoreCase (TERMINAL_KEYWORDS) (_.last (splitWithTerminal[0]))) {
+        return {'Remaining': splitWithTerminal[1],
+                'Parsed': {'Type': STATEMENT,
+                           'Values':
+                           _.concat (_.map (function (token) {return {'Type': OBJECT,
+                                                                      'Value': token};},
+                                            _.slice (0, splitWithTerminal[0].length - 1,
+                                                     splitWithTerminal[0]))) ([{'Type': TERMINAL,
+                                                                                'Value': _.last (splitWithTerminal [0])}])}};
     }
+    return {'Remaining': tokens,
+            'Parsed': undefined};
 }
 
 /* Keep in mind that the following three functions should be pure.  It
@@ -248,7 +296,7 @@ function respond (reply, value) {
  * please :)
  */
 function evaluateGreeting (greeting) {
-    return "";
+    return "Found greeting";
 }
 
 /* ComplexObject -> String
@@ -257,7 +305,7 @@ function evaluateGreeting (greeting) {
  * please :)
  */
 function evaluateStatement (statement) {
-    return "";
+    return "Found statement";
 }
 
 /* ComplexObject -> String
@@ -266,10 +314,18 @@ function evaluateStatement (statement) {
  * please :)
  */
 function evaluateQuestion (question) {
-    return "";
+    return "Evaluate question";
 }
 
 module.exports = {initialGreeting: initialGreeting,
                   tokenise: tokenise,
                   parseGreeting: parseGreeting,
-                  reply: reply};
+                  parseQuestion: parseQuestion,
+                  parseStatement: parseStatement,
+                  reply: reply,
+                  GREETING: GREETING,
+                  STATEMENT: STATEMENT,
+                  QUESTION: QUESTION,
+                  KEYWORD: KEYWORD,
+                  OBJECT: OBJECT,
+                  TERMINAL: TERMINAL};
